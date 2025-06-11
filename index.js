42b6c85b1584b6bbcc3794eacab31e415fbac124
Manas Uniyal <uniyalmanas@gmail.com>
2025-06-11 18:39:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,6 +14,28 @@ app.get('/login',(req,res)=>{
     res.send(' <h1>login page</h1> ')
 })
 
+const githubdata = {
+  "username": "uniyalmanas",
+  "email": "uniyalmanas@gmail.com",
+  "sshKeyType": "ed25519",
+  "sshKeyPath": "~/.ssh/id_ed25519",
+  "repository": {
+    "name": "ChaiBackend",
+    "url": "git@github.com:uniyalmanas/ChaiBackend.git",
+    "branch": "main",
+    "remote": "origin"
+  },
+  "lastCommit": {
+    "message": "Initial commit",
+    "date": "2025-06-11",
+    "author": "Manas Uniyal"
+  }
+}
+
+
+app.get('/github',(req,res)=>{
+ res.json(githubdata)
+})
 // Start the server
 app.listen(process.env.PORT, () => {
     console.log(`Listening to port ${port}`)
